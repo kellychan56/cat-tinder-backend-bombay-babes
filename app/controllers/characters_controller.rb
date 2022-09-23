6062d61fda0d508ef2958ef271f9318fb2f5991e
kellychan56 <kelly.chan568@gmail.com>
2022-09-23 10:10:49 -0700

    completed api endpoints for index, create, update, destroy, all tests working

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,2 +1,41 @@
 class CharactersController < ApplicationController
+    def index
+        characters = Character.all
+        render json: characters
+    end
+
+    def create
+        character = Character.create(character_params)
+        if character.valid?
+            render json: character
+        else
+            render json: character.errors, status: 422
+        end
+    end
+
+    def update
+        character = Character.find(params[:id])
+        character.update(character_params)
+        if character.valid?
+            render json: character
+        else
+            render json: character.errors, status: 422
+        end
+    end
+
+    def destroy
+        character = Character.find(params[:id])
+        if character.destroy
+            render json: character, status: 204
+        else
+            render json: character.errors
+        end
+    end
+
+    private
+
+    def character_params
+        params.require(:character).permit(:name, :age, :interests, :image, :image_alt)
+    end
+
 end
--- spec/requests/characters_spec.rb
+++ spec/requests/characters_spec.rb
@@ -1,7 +1,95 @@
 require 'rails_helper'
 
 RSpec.describe "Characters", type: :request do
+
+
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "get a list of all the characters" do
+      Character.create(
+        name: 'Lulu',
+        age: 22,
+        interests: 'Black, dolls, curry',
+        image: 'https://oyster.ignimgs.com/mediawiki/apis.ign.com/final-fantasy-x/2/25/Lulu.jpg',
+        image_alt: 'Picture of Lulu'
+      )
+
+      get '/characters'
+
+      characters = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(characters.length).to eq(1)
+    end
+  end
+
+  describe "POST /create" do
+    it "creates a new character" do
+      character_params = {
+        character: {
+          name: 'Lulu',
+          age: 22,
+          interests: 'Black, dolls, curry',
+          image: 'https://oyster.ignimgs.com/mediawiki/apis.ign.com/final-fantasy-x/2/25/Lulu.jpg',
+          image_alt: 'Picture of Lulu'
+        }
+      }
+
+      post '/characters', params: character_params
+
+      character = Character.first
+
+      expect(response).to have_http_status(200)
+      expect(character.name).to eq('Lulu')
+      expect(character.image_alt).to_not be_empty
+    end
+  end
+
+  describe "PATCH /update" do
+    it "updates an exisiting character" do
+      Character.create(
+        name: 'Lulu',
+        age: 22,
+        interests: 'Black, dolls, curry',
+        image: 'https://oyster.ignimgs.com/mediawiki/apis.ign.com/final-fantasy-x/2/25/Lulu.jpg',
+        image_alt: 'Picture of Lulu'
+      )
+
+      character = Character.first
+
+      character_params = {
+        character: {
+          age: 21,
+          image_alt: "Lulu pic"
+        }
+      }
+
+      patch "/characters/#{character.id}", params: character_params
+
+      character = Character.first
+
+      expect(character.age).to eq(21)
+      expect(character.image_alt).to_not eq("Picture of Lulu")
+      expect(character.image_alt).to eq("Lulu pic")
+    end
+  end
+
+  describe "DELETE /destory" do
+    it "deletes an exisiting character" do
+      Character.create(
+        name: 'Lulu',
+        age: 22,
+        interests: 'Black, dolls, curry',
+        image: 'https://oyster.ignimgs.com/mediawiki/apis.ign.com/final-fantasy-x/2/25/Lulu.jpg',
+        image_alt: 'Picture of Lulu'
+      )
+
+      character = Character.first
+
+      delete "/characters/#{character.id}"
+
+      expect(response.status).to eq(204)
+
+      characters = Character.all
+      expect(characters).to be_empty
+    end
   end
 end
